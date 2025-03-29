e5665563976d403c340190f5c3754f69bf33b4ed
NOOB-3301 <sepic466@gmail.com>
2025-03-29 13:11:49 +0530

    feat: integrate react-router-dom for routing in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './Components/LandingPage/LandingPage'
+import { Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <LandingPage/>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
     </>
   )
 }
